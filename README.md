6f2111855feefbf3f6b04cbb7e6d33c633d0dacf
hunter32292 <jrstupkadev@gmail.com>
2019-09-03 10:03:06 -0700

    Updated to react

--- README.md
+++
@@ -1,8 +0,0 @@
-# hunter32292.github.io
-
-A simple personal site running on github pages, currently running all static html pages.
-
-### todo
- - Add javascript framework and simplify design (Done)
- - Use travis CI or another free CI tool to handle code linting and dependancy checking
- - Feed blog through backend API to javascript frontend
---
+++ precache-manifest.7a43bfaf7b758525ddd10015cf7b6965.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "c0cb0abbf5b484f3401e",
+    "url": "/static/css/main.bb79e3a8.chunk.css"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "fabf32a3b8ae5b2e3830",
+    "url": "/static/js/1.fabf32a3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a1287353d47c98b9539fbe676670e70b",
+    "url": "/static/media/profile.a1287353.jpg"
+  },
+  {
+    "revision": "aab587895ba804f4b9f4bf1673938b84",
+    "url": "/static/media/joynt.aab58789.jpg"
+  },
+  {
+    "revision": "0661db71c218b7133688734ff14ee67c",
+    "url": "/static/media/racys.0661db71.jpg"
+  },
+  {
+    "revision": "ef0e1aa03ad04761a0bb645d748dac59",
+    "url": "/static/media/starwars.ef0e1aa0.jpg"
+  },
+  {
+    "revision": "a1affa2a84c37123a49ff5b2d26862fb",
+    "url": "/static/media/hackathon.a1affa2a.jpg"
+  },
+  {
+    "revision": "fa3a113ef361c09888cceef47fcccce0",
+    "url": "/static/media/old_website.fa3a113e.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "c0cb0abbf5b484f3401e",
+    "url": "/static/js/main.c0cb0abb.chunk.js"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "fabf32a3b8ae5b2e3830",
+    "url": "/static/css/1.e8817c4e.chunk.css"
+  },
+  {
+    "revision": "ba0ea7c188d9c07d84964abfc2c74480",
+    "url": "/index.html"
+  }
+];
